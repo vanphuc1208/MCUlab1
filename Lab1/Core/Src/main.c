5d9ff10c675199cec33e6a1c086519ecce558453
vanphuc1208 <levanphucdn2003@gmail.com>
2023-09-20 15:09:50 +0700

    commit

--- Lab1/Core/Src/main.c
+++ Lab1/Core/Src/main.c
@@ -97,9 +97,9 @@ int main(void)
     while (1)
     {
      if(timer1_flag==0) {
-  	   HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
-  	   HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin,GPIO_PIN_SET);
-  	   HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+    	 HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+    	 HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin,GPIO_PIN_SET);
+    	 HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
      }
 
      else if(timer2_flag==0) {
